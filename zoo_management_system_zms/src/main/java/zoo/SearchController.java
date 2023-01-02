7e456f06f6c80f4feded53f9418aa7275f3310ba
Ahmed Youssef <ahmedyoussefay022@gmail.com>
2023-01-02 02:21:58 +0200

    Controllers had finished

--- zoo_management_system_zms/src/main/java/zoo/SearchController.java
+++ zoo_management_system_zms/src/main/java/zoo/SearchController.java
@@ -15,53 +15,100 @@ import javafx.stage.Stage;
 
 public class SearchController extends AnimalDetalisController {
 
-    
-    
     @FXML
     TextField searchfield;
-    
+
+    String searchValue;
 
     
-   
+    private void search() {
+
+        if (searchfield.getText().equals("Lion")) {
+
+            searchValue = "Lion";
+            // return searchValue;
+            // System.out.println(1);
+
+        } else if (searchfield.getText().equals("Monkey")) {
+
+            searchValue="Monkey";
+            // return  searchValue;
+            // System.out.println(2);
+
+
+        } else if (searchfield.getText().equals("Giraffe")) {
+
+            searchValue="Giraffe";
+            // return searchValue;
+            // System.out.println(3);
+
+
+
+        } else if (searchfield.getText().equals("Tiger")) {
+
+            searchValue="Tiger";
+            // return searchValue;
+            // System.out.println(4);
+
+
+        }else if (searchfield.getText().equals("Zebra")) {
+            
+            searchValue="Zebra";
+            // return searchValue;
+            // System.out.println(5);
+
+
+        }
+         else {
+
+            // return "not found";
+            // System.out.println(6);
+            searchValue="unknown";
+
+
+        }
+
+    }
 
 
 
     @FXML
     private void switchToAnimal(ActionEvent event) throws IOException {
 
-       
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("animalDetails.fxml"));
-        Parent root=loader.load();
+        search();
 
-        AnimalDetalisController animalDetalisController=loader.getController();
-        animalDetalisController.setAnimalDetails();
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene=new Scene(root,600,400);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("animalDetails.fxml"));
+        Parent root = loader.load();
+
+        AnimalDetalisController animalDetalisController = loader.getController();
+        animalDetalisController.setAnimalDetails(searchValue);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
-        
+
     }
-    
+
     @FXML
     private void switchToSearch(ActionEvent event) throws IOException {
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("search.fxml"));
-        Parent root=loader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("search.fxml"));
+        Parent root = loader.load();
 
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene=new Scene(root,600,400);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
-    
-   
+
     @FXML
     private void switchToHomePage(ActionEvent event) throws IOException {
-   
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("homePage.fxml"));
-        Parent root=loader.load();
 
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene=new Scene(root,600,400);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("homePage.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
 
@@ -69,11 +116,11 @@ public class SearchController extends AnimalDetalisController {
 
     @FXML
     private void switchToIickets(ActionEvent event) throws IOException {
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("tickets.fxml"));
-        Parent root=loader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("tickets.fxml"));
+        Parent root = loader.load();
 
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene=new Scene(root,600,400);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
 
@@ -81,23 +128,22 @@ public class SearchController extends AnimalDetalisController {
 
     @FXML
     private void switchToLoginPage(ActionEvent event) throws IOException {
-    
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("loginPage.fxml"));
-        Parent root=loader.load();
 
-        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene=new Scene(root,680,400);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 680, 400);
         stage.setScene(scene);
         stage.show();
 
-   }
+    }
 
-   @FXML 
-   Label usersearch;
-   
-   public void welcomeuser(String wellcome){
-    usersearch.setText(wellcome);
-   }
+    @FXML
+    Label usersearch;
 
+    public void welcomeuser(String wellcome) {
+        usersearch.setText(wellcome);
+    }
 
 }
